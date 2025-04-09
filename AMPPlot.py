d87ef229c03eed5e3eacd6ebd0b4c76bd5d0af93
Lukas Petersen <lukas.petersen@kit.edu>
2025-04-09 22:37:11 +0200

    Refactor AMPPlot script to streamline plotting functions and improve code organization; implement a generic plotting function and enhance argument parsing

--- AMPPlot.py
+++ AMPPlot.py
@@ -1,19 +1,13 @@
 #!/lustre/home/ka/ka_ipc/ka_he8978/miniconda3/envs/mace_env/bin/python3.12
 import argparse
 from ase.io import read
-import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-# REF_GEOMS = "/lustre/work/ws/ws1/ka_he8978-dipeptide/training_data/B3LYP_aug-cc-pVTZ_vacuum/geoms.extxyz"
-AMP_GEOMS = "results/amp_qmmm_geoms.extxyz" # Should already contain reference and AMP data
+# Default geometry file
+AMP_GEOMS = "results/amp_qmmm_geoms.extxyz"
 
-PLOT_ENERGY = True
-PLOT_FORCES = True 
-PLOT_DIPOLE = True
-PLOT_QUADRUPOLE = True
-
-# The following keywords are used to extract the data from the ASE atoms object
+# Keywords for extracting data
 REF_ENERGY_KEY = "qm_energies_ref"
 REF_FORCES_KEY = "qm_gradients_ref"
 REF_DIPOLE_KEY = "dipole_ref"
@@ -26,90 +20,55 @@ PRED_QUADRUPOLE_KEY = "quadrupole_pred"
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Plotting script for AMP")
     parser.add_argument("-g", "--geoms", type=str, default=AMP_GEOMS, help="Path to the geometry file")
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
 
-def get_ref(
-        mols,
-        energy_keyword=None,
-        forces_keyword=None,
-        dipole_keyword=None,
-        quadrupole_keyword=None):
-    
+def get_ref(mols, energy_keyword=None, forces_keyword=None, dipole_keyword=None, quadrupole_keyword=None):
     ref_energy = []
     ref_forces = []
     ref_dipoles = []
     ref_quadrupoles = []
     for m in mols:
-        if dipole_keyword != None:
-                ref_dipoles.extend(m.info[dipole_keyword].flatten())
-        if energy_keyword != None:
+        if dipole_keyword:
+            ref_dipoles.extend(m.info[dipole_keyword].flatten())
+        if energy_keyword:
             if energy_keyword == "energy":
                 ref_energy.append(m.get_potential_energy())
             else:
                 ref_energy.append(m.info[energy_keyword])
-        if forces_keyword != None:
+        if forces_keyword:
             if forces_keyword == "forces":
                 ref_forces.extend(m.get_forces().flatten())
             else:
                 ref_forces.extend(m.arrays[forces_keyword].flatten())
-        if quadrupole_keyword != None:
+        if quadrupole_keyword:
             ref_quadrupoles.extend(m.info[quadrupole_keyword].flatten())
-    ref_energy = np.array(ref_energy)
-    ref_forces = np.array(ref_forces)
-    ref_dipoles = np.array(ref_dipoles)
-    ref_quadrupoles = np.array(ref_quadrupoles)
     return {
-        "energy": ref_energy, 
-        "forces": ref_forces,
-        "dipole": ref_dipoles,
-        "quadrupole": ref_quadrupoles,
-        }
-args = parse_args()
-amp_mols = read(args.geoms, format="extxyz", index=":")
-ref_data = get_ref(amp_mols, REF_ENERGY_KEY, REF_FORCES_KEY, REF_DIPOLE_KEY, REF_QUADRUPOLE_KEY)
-AMP_data = get_ref(amp_mols, PRED_ENERGY_KEY, PRED_FORCES_KEY, PRED_DIPOLE_KEY, PRED_QUADRUPOLE_KEY)
+        "energy": np.array(ref_energy),
+        "forces": np.array(ref_forces),
+        "dipole": np.array(ref_dipoles),
+        "quadrupole": np.array(ref_quadrupoles),
+    }
 
-if PLOT_ENERGY:
-    plt.scatter(ref_data["energy"], AMP_data["energy"], c='blue', alpha=0.5, label='Data Points')  # Scatter plot
-    plt.plot(ref_data["energy"], ref_data["energy"], color="black", label='Identity Line')  # Identity line
-    plt.xlabel('Ref energy')  # X-axis Label
-    plt.ylabel('AMP energy')  # Y-axis Label
-    plt.tight_layout()  # Tight layout for nicer appearance
-    plt.savefig("AMPenergy.png", dpi=300)
-    # plt.show()
+def plot_data(ref_data, pred_data, key, xlabel, ylabel, filename):
+    """Generic function to plot reference vs predicted data."""
+    plt.scatter(ref_data[key], pred_data[key], c='blue', alpha=0.5, label='Data Points')  # Scatter plot
+    plt.plot(ref_data[key], ref_data[key], color="black", label='Identity Line')  # Identity line
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.tight_layout()
+    plt.savefig(filename, dpi=300)
     plt.close()
 
-if PLOT_FORCES:
-    plt.scatter(ref_data["forces"], AMP_data["forces"], c='blue', alpha=0.5, label='Data Points')  # Scatter plot
-    plt.plot(ref_data["forces"], ref_data["forces"], color="black", label='Identity Line')  # Identity line
-    plt.xlabel('Ref forces')  # X-axis Label
-    plt.ylabel('AMP forces')  # Y-axis Label
-    plt.tight_layout()  # Tight layout for nicer appearance
-    plt.savefig("AMPforces.png", dpi=300)
-    #plt.show()
-    plt.close()
+def main():
+    args = parse_args()
+    amp_mols = read(args.geoms, format="extxyz", index=":")
+    ref_data = get_ref(amp_mols, REF_ENERGY_KEY, REF_FORCES_KEY, REF_DIPOLE_KEY, REF_QUADRUPOLE_KEY)
+    AMP_data = get_ref(amp_mols, PRED_ENERGY_KEY, PRED_FORCES_KEY, PRED_DIPOLE_KEY, PRED_QUADRUPOLE_KEY)
 
-if PLOT_DIPOLE:
-    plt.scatter(ref_data["dipole"], AMP_data["dipole"], c='blue', alpha=0.5, label='Data Points')  # Scatter plot
-    plt.plot(ref_data["dipole"], ref_data["dipole"], color="black", label='Identity Line')  # Identity line
-    plt.xlabel('Ref dipole')  # X-axis Label
-    plt.ylabel('AMP dipole')  # Y-axis Label
-    plt.tight_layout()  # Tight layout for nicer appearance
-    plt.savefig("AMPdipole.png",dpi=300)
-    #plt.show()
-    plt.close()
+    plot_data(ref_data, AMP_data, "energy", "Ref energy", "AMP energy", "AMPenergy.png")
+    plot_data(ref_data, AMP_data, "forces", "Ref forces", "AMP forces", "AMPforces.png")
+    plot_data(ref_data, AMP_data, "dipole", "Ref dipole", "AMP dipole", "AMPdipole.png")
+    plot_data(ref_data, AMP_data, "quadrupole", "Ref quadrupole", "AMP quadrupole", "AMPquadrupole.png")
 
-    
-if PLOT_QUADRUPOLE:
-    plt.scatter(ref_data["quadrupole"], AMP_data["quadrupole"], c='blue', alpha=0.5, label='Data Points')  # Scatter plot
-    plt.plot(ref_data["quadrupole"], ref_data["quadrupole"], color="black", label='Identity Line')  # Identity line
-    plt.xlabel('Ref quadrupole')  # X-axis Label
-    plt.ylabel('AMP quadrupole')  # Y-axis Label
-    plt.tight_layout()  # Tight layout for nicer appearance
-    plt.savefig("AMPquadrupole.png", dpi=300)
-    #plt.show()
-    plt.close()
-
-
-
+if __name__ == "__main__":
+    main()
